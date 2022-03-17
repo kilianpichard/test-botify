1ae08af2bdfff8c735a49b37e94c1be6d8e0e722
Kilian Pichard <perso@kilianp.fr>
2022-03-17 12:06:38 +0100

    add switch between table and graph

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from "react";
+import DataTable from 'react-data-table-component';
 
 function BarChart ({google,data}) {
     const [chart, setChart] = useState(null);
-    const [selected, setSelected] = useState(null);
-
+    const [selected, setSelected] = useState("");
+    const [displayed, setDisplayed] = useState("");
+    const [chartDisplay, setChartDisplay] = useState(true);
     const handleSelect = async (event) => {
         setSelected(event.target.value);
     };
 
+    const columns = [
+        {
+            name: 'Name',
+            selector: row => row[0],
+        },
+        {
+            name: 'Min estimated Diameter (Km)',
+            selector: row => row[1],
+        },
+        {
+            name: 'Max estimated Diameter (Km)',
+            selector: row => row[2],
+        },
+    ];
+
     useEffect(() => {
         if (google && !chart && data) {
 
-            let displayed = data.objects.filter(obj => selected === null || obj[3] === selected);
+            let mapped = data.objects.filter(obj => selected === "" || obj[3] === selected);
 
-            displayed = displayed.map(item => {
+            mapped = mapped.map(item => {
                 return [item[0], item[1], item[2]];
             });
 
             let table = google.visualization.arrayToDataTable([
                 ['Name', 'Min estimated diameter (km)', 'Max estimated diameter (km)'],
-                ...displayed
+                ...mapped
             ]);
+
+            setDisplayed(mapped);
 
             var options = {};
 
@@ -33,6 +52,7 @@ function BarChart ({google,data}) {
 
     return (
         <>
+            <button onClick={() => setChartDisplay(!chartDisplay)}>Change Display</button>
             {!chart && <h1>Loading Data...</h1>}
                 <select
                     value={selected}
@@ -41,7 +61,12 @@ function BarChart ({google,data}) {
                         <option key={o} value={o}>{o}</option>
                     ))}
                 </select>
-            <div id="BarChart" style={{height: '100vh',width: '100vw'}}/>
+
+            {chartDisplay.valueOf()}
+
+            <div id="BarChart" style={{height: '100vh',width: '100vw',display:`${chartDisplay ? "block" : "none"}`}}/>
+            {!chartDisplay && displayed && <DataTable columns={columns} data={displayed} />}
+
         </>
     )
 }
